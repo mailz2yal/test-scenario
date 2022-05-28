3ff23ebf46b0fe033a90a601e2b03cc75b19bba8
user <user@users-MacBook-Pro.local>
2022-05-27 19:17:32 -0700

    the second commit

--- src/test/java/com/assignmentseven/GmailVerfyAccount.java
+++ src/test/java/com/assignmentseven/GmailVerfyAccount.java
@@ -1,20 +1,11 @@
 package com.assignmentseven;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
